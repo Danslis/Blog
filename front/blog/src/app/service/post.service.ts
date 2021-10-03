3742e1ffce9d1329dea348d1b52aa132b42efaba
Денис Кирилин <d.kirilin@z-it.ru>
2021-10-03 20:19:30 +0300

    описал сервисы на фронте, создал необходимы компоненты #12

---
+++ front/blog/src/app/service/post.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { Post } from '../interfaces/postResponse';
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+  baseUrl: string;
+  constructor(private http: HttpClient) {
+    this.baseUrl = "http://localhost:5000";
+  }
+
+  getAll(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.baseUrl}/blog/get-posts`)
+  }
+
+  getPostById(id: number) {
+    return this.http.get(`${this.baseUrl}/blog/get-posts-by-id` + '/' + id);
+  }
+
+  createPost(post: Post) {
+    return this.http.post(`${this.baseUrl}/blog/create/`, post);
+}
+  updatePost(post: Post) {
+      return this.http.put(`${this.baseUrl}/blog/update/`, post);
+  }
+  deletePost(id: number) {
+      return this.http.delete(`${this.baseUrl}/blog/delete/` + '/' + id);
+  }
+}
